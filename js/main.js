356aa9786b9ac1ce952e1d6e77160274a48fd9a9
Recrus <dan.plishkin@gmail.com>
2023-10-20 16:25:58 +0300

    feat&fix: adds news, refactor js and scss

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import watch from "../img/watch.png";
-import toy from "../img/pc.png";
-
-document.addEventListener("DOMContentLoaded", initSlider);
+import pc from "../img/pc.png";
+import eye from "../img/eye.svg";
+import news1 from "../img/news1.png";
+import news2 from "../img/news2.png";
+import leo from "../img/leo.png";
 
 const slideData = [
   {
@@ -19,7 +21,7 @@ const slideData = [
   {
     title: "Другой пример,",
     subTitle: "Помогли компании NuxtPC увеличить количество подписчиков и продажи",
-    imgSrc: toy,
+    imgSrc: pc,
     backgroundColor: "#AEDB23",
     clientName: "NuxtPC",
     clientLink: "#",
@@ -29,23 +31,64 @@ const slideData = [
     ]
   }
 ];
+const newsSlideData = [
+  {
+    mainImage: news1,
+    authorAvatar: leo,
+    authorName: "Леонид Дикаприй",
+    views: 1357,
+    title: "27 способов рассказать о проблемах в вашем аккаунте",
+    link: "#"
+  },
+  {
+    mainImage: news2,
+    authorAvatar: leo,
+    authorName: "Леонид Дикаприй",
+    views: 701,
+    title: "2 способа завоевать доверие",
+    link: "#"
+  },
+  {
+    mainImage: news1,
+    authorAvatar: leo,
+    authorName: "Леонид Дикаприй",
+    views: 1232,
+    title: "27 способов рассказать о проблемах в вашем аккаунте",
+    link: "#"
+  },
+  {
+    mainImage: news2,
+    authorAvatar: leo,
+    authorName: "Леонид Дикаприй",
+    views: 2242,
+    title: "2 способа рассказать о проблемах в вашем аккаунте",
+    link: "#"
+  }
+];
 
+document.addEventListener("DOMContentLoaded", () => {
+  initSlider('.case-slider__wrapper', slideData, createCaseSlide, navigateCaseSlide);
 
-function initSlider() {
-  slideData.forEach(createSlide);
-  let slideIndex = 0;
+  const sortedNews = newsSlideData.sort((a, b) => b.views - a.views);
+  initSlider('.news-slider__items', sortedNews, createNewsSlide, navigateNewsSlide);
+});
 
-  document.querySelector('.next-slide').addEventListener('click', () => {
-    slideIndex = navigateSlide(++slideIndex);
-  });
+function initSlider(containerSelector, data, slideCreator, navigator) {
+  data.forEach(slideCreator);
 
-  document.querySelector('.prev-slide').addEventListener('click', () => {
-    slideIndex = navigateSlide(--slideIndex);
-  });
+  const nextButton = document.querySelector(`${containerSelector}-next`);
+  const prevButton = document.querySelector(`${containerSelector}-prev`);
 
-  navigateSlide(slideIndex);
+  if (nextButton && prevButton) {
+    let slideIndex = 0;
+    nextButton.addEventListener('click', () => navigator(++slideIndex));
+    prevButton.addEventListener('click', () => navigator(--slideIndex));
+
+    navigator(slideIndex);
+  }
 }
 
+// Utility function to create elements
 function createElement(tag, classNames = [], attributes = {}) {
   const element = document.createElement(tag);
   element.classList.add(...classNames);
@@ -55,79 +98,175 @@ function createElement(tag, classNames = [], attributes = {}) {
   return element;
 }
 
-function createSlide(slide) {
-  const sliderWrapper = document.querySelector('.slider-wrapper');
+function createCaseSlide(slide) {
+  const sliderWrapper = document.querySelector('.case-slider__wrapper');
+  if (!sliderWrapper) return;
 
-  const slideDiv = createElement('div', ['slide'], {style: `background-color: ${slide.backgroundColor}`});
-  const contentDiv = createElement('div', ['slide__content']);
-  const h2 = createElement('h2');
-  h2.innerHTML = `<span>${slide.title}</span><br>${slide.subTitle}`;
-  const img = createElement('img', [], {src: slide.imgSrc, alt: slide.title});
+  const slideElement = createElement('div', ['slide'], { style: `background-color: ${slide.backgroundColor}` });
+  const contentElement = createElement('div', ['slide__content']);
 
-  contentDiv.append(h2, img);
-  slideDiv.appendChild(contentDiv);
-  sliderWrapper.appendChild(slideDiv);
+  const titleElement = createElement('h2');
+  titleElement.innerHTML = `<span>${slide.title}</span><br>${slide.subTitle}`;
+
+  const imageElement = createElement('img', [], { src: slide.imgSrc, alt: slide.title });
+
+  contentElement.append(titleElement, imageElement);
+  slideElement.appendChild(contentElement);
+  sliderWrapper.appendChild(slideElement);
 }
 
-function updateSlideText(index) {
-  const slide = slideData[index];
-  const slideTextContainer = document.querySelector('.slide-text');
-  slideTextContainer.innerHTML = '';
+function navigateCaseSlide(index) {
+  const newIndex = generalNavigate(index, '.case-slider__wrapper', '.slide', slideData.length);
+  updateCaseSlideText(newIndex);
+  return newIndex;
+}
 
+function updateCaseSlideText(index) {
+  const slide = slideData[index];
+
+  const slideTextContainer = document.querySelector('.slide-text');
+  if (!slideTextContainer) return;
+
+  const slideTextClient = slideTextContainer.querySelector(".slide-text__client");
+  const slideTextStats = slideTextContainer.querySelector(".slide-text__stats-wrapper");
+
+  if (slideTextClient && slideTextStats) {
+    fadeOut(slideTextClient);
+    fadeOut(slideTextStats);
+  }
+
+  setTimeout(() => {
+    slideTextContainer.innerHTML = '';
+
+    const clientElement = createClientElement(slide);
+    slideTextContainer.appendChild(clientElement);
+
+    const dividerElement = createDivider();
+    slideTextContainer.appendChild(dividerElement);
+
+    const statsElement = createStatsElement(slide);
+    slideTextContainer.appendChild(statsElement);
+
+    slideTextContainer.appendChild(dividerElement.cloneNode());
+
+    if (slideTextClient && slideTextStats) {
+      fadeIn(slideTextClient);
+      fadeIn(slideTextStats);
+    }
+
+  }, 300);
+}
+
+function createClientElement(slide) {
   const clientDiv = createElement('div', ['slide-text__client']);
+
   const clientTitle = createElement('p', ['slide-text__client-title']);
   clientTitle.innerHTML = `Клиент: <span class="slide-text__client-name">${slide.clientName}</span>`;
-  const clientLink = createElement('a', ['slide-text__client-link'], {href: slide.clientLink});
+
+  const clientLink = createElement('a', ['slide-text__client-link'], { href: slide.clientLink });
   clientLink.innerText = slide.clientName;
-  const otherWorksLink = createElement('a', ['slide-text__other-works'], {href: "#"});
+
+  const otherWorksLink = createElement('a', ['slide-text__other-works'], { href: "#" });
   otherWorksLink.innerText = "другие работы";
 
   clientDiv.append(clientTitle, clientLink, otherWorksLink);
-  slideTextContainer.appendChild(clientDiv);
-
-  const divider = document.createElement('hr');
-  divider.className = "slide-text__divider";
-  slideTextContainer.appendChild(divider);
-
-  const statsContainer = document.createElement('div');
-  statsContainer.className = "slide-text__stats-wrapper";
-  slideTextContainer.appendChild(statsContainer);
-
-  slide.stats.forEach(stat => {
-    const statsDiv = document.createElement('div');
-    statsDiv.className = "slide-text__stats";
-
-    const h2 = document.createElement('h2');
-      h2.className = "slide-text__stats-value";
-      h2.innerText = stat.value;
-      statsDiv.appendChild(h2);
-
-      const p = document.createElement('p');
-      p.className = "slide-text__stats-description";
-      p.innerText = stat.description;
-      statsDiv.appendChild(p);
-
-      statsContainer.appendChild(statsDiv);
-    });
-
-    const divider2 = document.createElement('hr');
-    divider2.className = "slide-text__divider";
-    slideTextContainer.appendChild(divider2);
-
+  return clientDiv;
 }
 
-function navigateSlide(index) {
-  console.log(index);
-  const sliderWrapper = document.querySelector('.slider-wrapper');
-  const slides = document.querySelectorAll('.slide');
+function createDivider() {
+  const divider = createElement('hr');
+  divider.className = "slide-text__divider";
+  return divider;
+}
 
-  if (index >= slides.length) index = 0;
-  if (index < 0) index = slides.length - 1;
+function createStatsElement(slide) {
+  const statsContainer = createElement('div', ['slide-text__stats-wrapper']);
 
-  const offset = -index * 100;
+  slide.stats.forEach(stat => {
+    const statsDiv = createElement('div', ['slide-text__stats']);
+
+    const h2 = createElement('h2', ['slide-text__stats-value']);
+    h2.innerText = stat.value;
+    statsDiv.appendChild(h2);
+
+    const p = createElement('p', ['slide-text__stats-description']);
+    p.innerText = stat.description;
+    statsDiv.appendChild(p);
+
+    statsContainer.appendChild(statsDiv);
+  });
+
+  return statsContainer;
+}
+
+function fadeOut(element) {
+  element.classList.add('fade-out');
+  element.classList.remove('fade-in');
+}
+
+function fadeIn(element) {
+  element.classList.remove('fade-out');
+  element.classList.add('fade-in');
+  setTimeout(() => {
+    element.classList.remove('fade-in');
+  }, 300);
+}
+
+
+function createNewsSlide(slide) {
+  const itemsContainer = document.querySelector('.news-slider__items');
+
+  const slideDiv = createElement('div', ['news-slider__item']);
+
+  const mainImage = createElement('img', ['news-slider__item-image'], {src: slide.mainImage, alt: slide.title});
+
+  const contentDiv = createElement('div', ['news-slider__item-content']);
+  const authorDiv = createElement('div', ['news-slider__author']);
+
+  const authorAvatar = createElement('img', ['news-slider__author-avatar'], {src: slide.authorAvatar, alt: slide.authorName});
+
+  const authorInfoDiv = createElement('div', ['news-slider__author-info']);
+  const authorName = createElement('h3', ['news-slider__author-name']);
+  authorName.textContent = slide.authorName;
+
+  const viewsDiv = createElement('div', ['news-slider__author-views']);
+  const eyeIcon = createElement('img', [], {src: eye, alt: "Views"});
+  const viewsCount = createElement('p');
+  viewsCount.textContent = slide.views;
+
+  viewsDiv.append(eyeIcon, viewsCount);
+  authorInfoDiv.append(authorName, viewsDiv);
+  authorDiv.append(authorAvatar, authorInfoDiv);
+
+  const itemTitle = createElement('h2', ['news-slider__item-title']);
+  itemTitle.textContent = slide.title;
+
+  const itemLink = createElement('a', ['news-slider__item-link'], {href: slide.link});
+  itemLink.textContent = ">>";
+
+  contentDiv.append(authorDiv, itemTitle, itemLink);
+  slideDiv.append(mainImage, contentDiv);
+
+  itemsContainer.appendChild(slideDiv);
+}
+
+function navigateNewsSlide(index) {
+  return generalNavigate(index, '.news-slider__items', '.news-slider__item', newsSlideData.length / 2, 50);
+}
+
+function generalNavigate(index, wrapperSelector, itemSelector, maxSlides, percentage = 100) {
+  const sliderWrapper = document.querySelector(wrapperSelector);
+  const slides = document.querySelectorAll(itemSelector);
+
+  if (index >= maxSlides) index = 0;
+  if (index < 0) index = maxSlides - 1;
+
+  const offset = -index * percentage;
   sliderWrapper.style.transform = `translateX(${offset}%)`;
 
-  updateSlideText(index);
+  if (wrapperSelector === '.case-slider__wrapper') {
+    updateCaseSlideText(index);
+  }
 
   return index;
 }
